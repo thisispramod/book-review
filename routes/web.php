2ff79716c84e959c53384a8cf8e6d29b28a5c83b
root <root_@todospeical-root>
2025-09-29 00:21:27 +0530

    added redirection when application load it goes to book index page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\BookController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 
 Route::resource('books', BookController::class);
